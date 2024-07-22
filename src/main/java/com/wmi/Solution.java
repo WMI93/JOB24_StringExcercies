34e699d1fe17bcc24f412803fbb745140b5d01a1
wmi93 <wertanimohamedilyes@gmail.com>
2024-07-22 22:21:02 +0200

    add Solution.Ex4_CharacterToASCII()  and Solution.Ex5_StrongToASCII() methods

--- src/main/java/com/wmi/Solution.java
+++ src/main/java/com/wmi/Solution.java
@@ -2,12 +2,16 @@ package com.wmi;
 
 import javax.xml.stream.events.Characters;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static com.wmi.Main.sop;
 
 public class Solution {
+    // Method 1 : reverse a input string
     static String Ex1_ReverseString(String s){
         int i=s.length();
         StringBuilder ss= new StringBuilder();
@@ -17,13 +21,14 @@ public class Solution {
         }
         return ss.toString();
     }
+    // Method 2 : reverse a input string
     static String Ex1_ReverseString1(String s){
         /*
             return  new StringBuilder(s).reverse().toString();
          */
         return Stream.of(s).map(ss->new StringBuilder(s).reverse()).collect(Collectors.joining(" "));
     }
-
+    //Display three-digit 1-4
     public static void Ex2_desplayDegit() {
         int i = 0, j = 0, k = 0, z = 0;
         for (i = 1; i <= 4; i++) {
@@ -39,18 +44,28 @@ public class Solution {
         }
         sop("z ==" + z);
     }
-
+    //list the available character sets in charset objects.
     public static void Ex3_AvailableCharset() {
         for(String s: Charset.availableCharsets().keySet()){
-            sop(" " + s);
+            sop(s);
         }
     }
-
-    public static String Ex4_CharacterToASCII(String string) {
-        return "";
+    //print a ASCII value of a given character
+    public static int Ex4_CharacterToASCII(String string) {
+        if(string.length()!=1)
+            return -1;
+        else
+            return string.charAt(0);
     }
 
-    public static String Ex5_StrongToASCII(String string) {
-        return "";
+    public static List<Integer> Ex5_StrongToASCII(String string) {
+        List<Integer> list = new ArrayList<>();
+        int i =0;
+        while(i<string.length()){
+            String s = String.valueOf(string.charAt(i));
+            list.add(Solution.Ex4_CharacterToASCII(s));
+            i++;
+        }
+        return list;
     }
 }
